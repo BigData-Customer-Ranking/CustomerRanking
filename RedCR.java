7e27c31d98054131da5752e806f75031c4adebc9
daneena-22 <35718418+daneena-22@users.noreply.github.com>
2018-03-15 21:24:59 +0530

    Update RedCR.java

--- RedCR.java
+++ RedCR.java
@@ -11,12 +11,9 @@ public class RedCR extends Reducer<Text,IntWritable,Text,IntWritable> {
 			for (IntWritable val : values) {
 				sum += val.get();
 			}
-			/*while(values.iterator().hasNext()){
-				IntWritable i=values.iterator().next();
-				sum+=i.get();
-			}*/
-			result.set(sum);
-			context.write(key, result);
+			
+		result.set(sum);
+		context.write(key, result);
 			 
 			
 			
